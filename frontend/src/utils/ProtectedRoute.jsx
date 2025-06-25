782aa2704459d4f953506bcaf06688ac7512acf5
Afzal Ansari <ansarahemad1220@gmail.com>
2025-06-25 23:33:44 +0530

    final commit

--- frontend/src/utils/ProtectedRoute.jsx
+++ frontend/src/utils/ProtectedRoute.jsx
@@ -1,19 +1,20 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect } from 'react';
 import { UserContext } from '../context/portfoliocontext';
 import { useNavigate } from 'react-router-dom';
 
-const ProtectedRoute = ({children}) => {
-  const {user} = useContext(UserContext);
-  const navigate=useNavigate();
-//  console.log(user);
-  if (!user) {
-    // alert("Login First");
-    // console.log(user);
-    return navigate('/');
-  }
+const ProtectedRoute = ({ children }) => {
+  const { user } = useContext(UserContext);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!user) {
+      navigate('/');
+    }
+  }, [user, navigate]);
+
+  if (!user) return null;
 
   return children;
 };
-
 
 export default ProtectedRoute;
